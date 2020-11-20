bd230729ddf447e2868feda5010db50e86660b9c
hasanince <alhasanince@gmail.com>
2020-11-20 14:58:30 +0300

    Update remocolab.py

--- remocolab.py
+++ remocolab.py
@@ -393,7 +393,7 @@ subprocess.run(
 (pathlib.Path.home() / ".xscreensaver").write_text("mode: off\\n")
 """)
   r = subprocess.run(
-                    ["su", "-c", "python3 " + str(vncrun_py), "colab"],
+                    ["su", "-c", "python3 " + str(vncrun_py), "hasanince"],
                     check = True,
                     stdout = subprocess.PIPE,
                     universal_newlines = True)
