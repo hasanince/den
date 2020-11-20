76a0065c9c865cb0408e73d17fd7b7d7d85eced0
hasanince <alhasanince@gmail.com>
2020-11-20 12:57:40 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "deneme.py",
+    version = "0.1",
+    py_modules = ['remocolab'],
+    url = "https://github.com/",
+    author = "hasanince",
+    install_requires = ["pyngrok"]
+)
